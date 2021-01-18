2b79cda65c0f10321ae2df712454ab424ebbaac2
chella1cm <chella1cm@gmail.com>
2021-01-19 00:57:19 +0530

    Update Youtube-dl.bat

--- Youtube-dl.bat
+++ Youtube-dl.bat
@@ -35,7 +35,7 @@ SET /P Start="Start Time (HH:MM:SS): "
 SET /P End="End Time (HH:MM:SS): "
 SET /P Name="Name (with Extension): "
 ECHO.
-for /f %%N in ('D:\Soft\Mpv\youtube-dl.exe -f %%format%% -g %%URL%%') do @ffmpeg -i "%%N" -ss %Start% -to %End% -c copy -copyts %Name%
+for /f %%N in ('D:\Soft\Mpv\youtube-dl.exe --youtube-skip-dash-manifest -f %%format%% -g %%URL%%') do @ffmpeg -i "%%N" -ss %Start% -to %End% -c copy -copyts C:\Users\Chella\Downloads\%Name%
 ECHO ----------------------------------------------------------------------------------------------------------------------
 ECHO.
 PAUSE
@@ -47,7 +47,7 @@ ECHO.
 ECHO ----------------------------------------------------------------------------------------------------------------------
 SET /P format="Select format: "
 ECHO.
-D:\Soft\Mpv\youtube-dl.exe -o Downloads/%%(title)s.%%(ext)s -f %format% -i --youtube-skip-dash-manifest --add-metadata --no-mtime --hls-prefer-native --write-auto-sub --write-sub --sub-lang en --embed-subs --embed-thumbnail --merge-output-format mp4 %URL%
+D:\Soft\Mpv\youtube-dl.exe -o C:\Users\Chella\Downloads/%%(title)s.%%(ext)s -f %format% -i --youtube-skip-dash-manifest --add-metadata --no-mtime --hls-prefer-native --write-auto-sub --write-sub --sub-lang en --embed-subs --embed-thumbnail --merge-output-format mp4 %URL%
 ECHO ----------------------------------------------------------------------------------------------------------------------
 ECHO.
 PAUSE
